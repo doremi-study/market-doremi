3ca5e3e708c30b1d4f8fd23bcf2ee879e428dd88
myeongjooseo <juux_x@naver.com>
2021-12-26 15:05:48 +0900

    init commit

---
+++ src/main/java/com/doremi/marketdoremi/MarketDoremiApplication.java
@@ -0,0 +1,13 @@
+package com.doremi.marketdoremi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDoremiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketDoremiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doremi/marketdoremi/MarketDoremiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doremi.marketdoremi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDoremiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

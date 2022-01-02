29da485a9c94268b6f92b6311ad632472d452461
myeongjooseo <juux_x@naver.com>
2022-01-02 21:27:55 +0900

    test: junit import 변경

--- src/test/java/com/doremi/marketdoremi/domain/member/MemberRepositoryTest.java
+++ src/test/java/com/doremi/marketdoremi/domain/member/MemberRepositoryTest.java
@@ -2,15 +2,13 @@ package com.doremi.marketdoremi.domain.member;
 
 import com.doremi.marketdoremi.domain.member.entity.Member;
 import com.doremi.marketdoremi.domain.member.repository.MemberRepository;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class MemberRepositoryTest {
 

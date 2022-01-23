2bdf91f47f03b0bf46c415cddef268e4ad0fc0ef
SYrongK <sYrongK@gmail.com>
2022-01-23 14:50:37 +0900

    refactor: RestControllerAdvice로 변경

--- src/main/java/com/doremi/marketdoremi/common/error/ErrorControllerAdvice.java
+++ src/main/java/com/doremi/marketdoremi/common/error/ErrorControllerAdvice.java
@@ -5,11 +5,11 @@ import com.doremi.marketdoremi.common.error.exceptions.DoremiRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ErrorControllerAdvice {
 
     @ExceptionHandler({DoremiException.class, DoremiRuntimeException.class})
